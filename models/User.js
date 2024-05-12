4346c4549c49b262d1f6c54b1d1373995f690f54
Keegan Micu <LemonTag73@gmail.com>
2024-05-12 15:32:57 -0400

    added more code into models folders

--- models/User.js
+++ models/User.js
@@ -1,60 +1,42 @@
-const { Model, DataTypes } = require('sequelize');
-const bcrypt = require('bcrypt');
-const sequelize = require('../config/connection');
-
-class User extends Model {
-  // Checks the password against the encrypted password in the database.
-  checkPassword(loginPw) {
-    return bcrypt.compareSync(loginPw, this.password);
-  }
-}
-
-User.init(
+[
   {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true,
-      },
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [8],
-      },
-    },
+    "username": "cady_heron",
+    "password": "password123"
   },
   {
-    // Hooks are used so that if a user is created or updated, the password is encrypted before being stored in the database.
-    hooks: {
-      beforeCreate: async (newUserData) => {
-        newUserData.password = await bcrypt.hash(newUserData.password, 10);
-        return newUserData;
-      },
-      beforeUpdate: async (updatedUserData) => {
-        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-        return updatedUserData;
-      },
-    },
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'user',
+    "username": "aaron_samuels",
+    "password": "password123"
+  },
+  {
+    "username": "regina_george",
+    "password": "password123"
+  },
+  {
+    "username": "gretchen_wieners",
+    "password": "password123"
+  },
+  {
+    "username": "janis_ian",
+    "password": "password123"
+  },
+  {
+    "username": "karen_smith",
+    "password": "password123"
+  },
+  {
+    "username": "damian",
+    "password": "password123"
+  },
+  {
+    "username": "coach_carr",
+    "password": "password123"
+  },
+  {
+    "username": "crying_girl",
+    "password": "password123"
+  },
+  {
+    "username": "testaccount",
+    "password": "password123"
   }
-);
-
-module.exports = User;
+]
